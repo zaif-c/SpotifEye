81b165002d58fd02fb6edca72fece0cff5f498a0
zaif-c <zaif.chowdhury@gmail.com>
2025-05-20 16:34:27 -0700

    Revert "Update API_URL to use environment variable instead of hardcoded value"
    This reverts commit 1404aec23c18f237a64eeda1558c6dcc9885c1e0.
    
    Reverting to working version

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -79,4 +79,3 @@ export const spotify = {
     return response.data;
   },
 };
- 
